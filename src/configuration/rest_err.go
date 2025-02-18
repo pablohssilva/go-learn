848ff10a76f6c54a591a6ab20ae857cca56a289d
pablohssilva <pablonfeiranduba@gmail.com>
2025-02-18 11:35:27 -0300

    aula2

---
+++ src/configuration/rest_err.go
@@ -0,0 +1,67 @@
+package rest_err
+
+type RestErr struct {
+	Message string  `json:"message"`
+	Err string 		`json:"error"`
+	Code int64  	`json:"code"`
+	Causes []Causes `json:"causes"`
+}
+
+tupe Causes struct {
+	Field string 	`json:"field"`
+	Message string 	`json:"message"`
+}
+
+func NewRestErr(message, err string, code int64, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
+
+func (r *RestErr) Error() string{
+	return r.Message
+}
