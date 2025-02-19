c27e5902b4e7da1ef8f561da00cd90fe16b0c415
pablohssilva <pablonfeiranduba@gmail.com>
2025-02-19 18:06:32 -0300

    referencia

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,14 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pablohssilva/go-learn/src/configuration/rest_err"
+)
 
 func CreateUser(c *gin.Context){
 
 
 	err := rest_err.NewBadRequestError("")
 	c.JSON(err.Code, err)
-	
+
 }
