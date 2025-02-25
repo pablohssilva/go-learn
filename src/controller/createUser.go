7a55e58aecc322a15aaa73bd4ecdbe0ac9ed638f
pablohssilva <pablonfeiranduba@gmail.com>
2025-02-25 15:36:30 -0300

    hjvai

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,14 +1,13 @@
 package controller
 
-import "github.com/gin-gonic/gin"
-
-func CreateUser(c *gin.Context){
->>>>>>> c27e590 (referencia)
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pablohssilva/go-learn/src/configuration/rest_err"
+)
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("")
+	err := rest_err.NewBadRequestError("Voce Chamou a rota de forma errada")
 	c.JSON(err.Code, err)
-	
+
 }
->>>>>>> c27e590 (referencia)
